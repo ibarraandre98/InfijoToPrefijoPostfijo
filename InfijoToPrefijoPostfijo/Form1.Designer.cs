32300a831eea82effecbfd81a2786db25b1b7c85
Andre Ibarra Perez <RayAsm1@hotmail.com>
2019-09-19 23:33:17 -0500

    Agregado de clases

---
+++ InfijoToPrefijoPostfijo/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace InfijoToPrefijoPostfijo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInfija = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrefija = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPostfija = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInfija
+            // 
+            this.txtInfija.Location = new System.Drawing.Point(100, 64);
+            this.txtInfija.Name = "txtInfija";
+            this.txtInfija.Size = new System.Drawing.Size(419, 20);
+            this.txtInfija.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Infija:";
+            // 
+            // txtPrefija
+            // 
+            this.txtPrefija.Enabled = false;
+            this.txtPrefija.Location = new System.Drawing.Point(100, 103);
+            this.txtPrefija.Name = "txtPrefija";
+            this.txtPrefija.Size = new System.Drawing.Size(419, 20);
+            this.txtPrefija.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prefija:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Postfija:";
+            // 
+            // txtPostfija
+            // 
+            this.txtPostfija.Enabled = false;
+            this.txtPostfija.Location = new System.Drawing.Point(100, 142);
+            this.txtPostfija.Name = "txtPostfija";
+            this.txtPostfija.Size = new System.Drawing.Size(419, 20);
+            this.txtPostfija.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(274, 186);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPostfija);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrefija);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInfija);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInfija;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrefija;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPostfija;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ InfijoToPrefijoPostfijo/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfijoToPrefijoPostfijo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            String cadena = txtInfija.Text;
+            operaciones(cadena);
+        }
+
+        private void operaciones(String cadena)
+        {
+            if (cadena == null || cadena.Length == 0)
+                return;
+
+            String cadenavolteada = VoltearCadena(cadena);
+            String cadenaPrefijaVolteada = CalcularPrefija(cadenavolteada);
+            String cadenaPrefija = VoltearCadena(cadenaPrefijaVolteada);
+            txtPrefija.Text = cadenaPrefija;
+
+
+            txtPostfija.Text = CalcularPostfija(cadenavolteada);
+        }
+
+        private String VoltearCadena(String cadena)
+        {
+            char[] arreglocadena = cadena.ToCharArray();
+            Array.Reverse(arreglocadena);
+            for (int i = 0; i <= arreglocadena.Length - 1; i++)
+            {
+                if (arreglocadena[i] == '(')
+                {
+                    arreglocadena[i] = ')';
+                }
+                else if (arreglocadena[i] == ')')
+                {
+                    arreglocadena[i] = '(';
+                }
+
+                else
+                {
+                    continue;
+                }
+            }
+            return new String(arreglocadena);
+        }
+
+        private String CalcularPrefija(String ExpresionInfija)
+        {
+
+            if (ExpresionInfija == null || ExpresionInfija.Length == 0)
+                return string.Empty;
+            StringBuilder strBuilder = new StringBuilder();
+            Stack<char> s = new Stack<char>();
+
+            for (int i = 0; i <= ExpresionInfija.Length - 1; i++)
+            {
+                if (ExpresionInfija[i] == '(')
+                {
+                    s.Push(ExpresionInfija[i]);
+                }
+                else if (ExpresionInfija[i] == ')')
+                {
+                    while (s.Count > 0 && s.Peek() != '(')
+                    {
+                        strBuilder.Append(s.Pop());
+                    }
+                    s.Pop();
+                }
+                else if (ExpresionInfija[i] == '+' || ExpresionInfija[i] == '-'
+                         || ExpresionInfija[i] == '/' || ExpresionInfija[i] == '*')
+                {
+
+                    while (s.Count > 0 && MismoOperador(ExpresionInfija[i], s.Peek()) == true)
+                    {
+                        strBuilder.Append(s.Pop());
+                    }
+                        
+
+                    while (s.Count > 0 && OperadorMayor(ExpresionInfija[i], s.Peek()) == true)
+                    {
+                        strBuilder.Append(s.Pop());
+                    }
+                        
+
+                    s.Push(ExpresionInfija[i]);
+                }
+                else
+                {
+                    strBuilder.Append(ExpresionInfija[i]);
+                }
+
+            }
+
+            while (s.Count != 0)
+            {
+                strBuilder.Append(s.Pop());
+            }
+            return strBuilder.ToString();
+        }
+
+        private String CalcularPostfija(String ExpresionInfija)
+        {
+            if (ExpresionInfija == null || ExpresionInfija.Length == 0)
+                return string.Empty;
+            MessageBox.Show(ExpresionInfija);
+            StringBuilder strBuilder = new StringBuilder();
+            Stack<char> s = new Stack<char>();
+
+            for (int i = ExpresionInfija.Length - 1; i >= 0; i--)
+            {
+                if (ExpresionInfija[i] == ')')
+                {
+                    s.Push(ExpresionInfija[i]);
+                }
+                else if (ExpresionInfija[i] == '(')
+                {
+                    while (s.Count > 0 && s.Peek() != ')')
+                    {
+                        strBuilder.Append(s.Pop());
+                    }
+                    s.Pop();
+                }
+                else if (ExpresionInfija[i] == '+' || ExpresionInfija[i] == '-'
+                         || ExpresionInfija[i] == '/' || ExpresionInfija[i] == '*')
+                {
+
+                    while (s.Count > 0 && MismoOperador(ExpresionInfija[i], s.Peek()) == true)
+                    {
+                        strBuilder.Append(s.Pop());
+                    }
+
+
+                    while (s.Count > 0 && OperadorMayor(ExpresionInfija[i], s.Peek()) == true)
+                    {
+                        strBuilder.Append(s.Pop());
+                    }
+
+
+                    s.Push(ExpresionInfija[i]);
+                }
+                else
+                {
+                    strBuilder.Append(ExpresionInfija[i]);
+                }
+
+            }
+
+            while (s.Count != 0)
+            {
+                strBuilder.Append(s.Pop());
+            }
+
+            return strBuilder.ToString();
+        }
+
+        private bool MismoOperador(char a,char b)
+        {
+            if ((a == '+' || a == '-') && (b == '+' || b == '-')) return true;
+            else if ((a == '*' || a == '/') && (b == '*' || b == '/')) return true;
+            else return false;
+        }
+
+        private bool OperadorMayor(char a,char b)
+        {
+            if ((a == '+' || a == '-') && (b == '*' || b == '/')) return true;
+            else return false;
+        }
+    }
+}
